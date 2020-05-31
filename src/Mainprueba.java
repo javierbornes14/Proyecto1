b4448ca234cf1b810cb171c51cada89febf1380f
javierbornes14 <javierbornesmoreno@gmail.com>
2020-05-31 05:35:24 +0200

    First commit

---
+++ src/Mainprueba.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Mainprueba {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double estatura;
+		
+		System.out.println("Nueva estatura: ");
+		estatura = scanner.nextDouble()
+		
+	}
+
+}
